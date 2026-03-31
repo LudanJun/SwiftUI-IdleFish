46bd0e838de68b514461f1cd0bbe2552b2c8b399
卤蛋君 <76891972@qq.com>
2026-03-31 17:16:44 +0800

    2.用贝尔塞尔曲线绘制凸起按钮

--- IdleFish/IdleFish/ContentView.swift
+++ IdleFish/IdleFish/ContentView.swift
@@ -13,20 +13,27 @@ struct ContentView: View {
     
     var body: some View {
         
-        //Zstack是一个z轴方向叠加的容器相当于Flutter的Stack
-        ZStack(alignment: .bottom){
-            //TabView相当于UITabbarViewController 粗浅对照
-            TabView(selection: $currTab){
-                //其中view相当于UITabBarViewController的rootViewController
-                ForEach(CustomTabbar.allCases,id:\.rawValue) {
-                    item in
-                    Text(item.rawValue)
-                        .tag(item)
+        GeometryReader { proxy in
+            //Zstack是一个z轴方向叠加的容器相当于Flutter的Stack
+            ZStack(alignment: .bottom){
+                //TabView相当于UITabbarViewController 粗浅对照
+                TabView(selection: $currTab){
+                    //其中view相当于UITabBarViewController的rootViewController
+                    ForEach(CustomTabbar.allCases,id:\.rawValue) {
+                        item in
+                        Text(item.rawValue)
+                            .tag(item)
+                        //为了方便调试UI 区分接线,添加一些背景色
+                            .frame(maxWidth: .infinity,maxHeight: .infinity)
+                            .background(Color.blue)
+                    }
                 }
+                //让CustomTabbarView相对底部有一个间距,这个距离是苹果定义的安全距离,如何获取
+                //创建自定义tabbarView,闲鱼有个凸起的圆弧 ,从这里传递绑定的属性
+                CustomTabbarView(safeEdgeInsets: proxy.safeAreaInsets, currTab: $currTab)
             }
-            
-            //创建自定义tabbarView,闲鱼有个凸起的圆弧 ,从这里传递绑定的属性
-            CustomTabbarView(currTab: $currTab)
+            //解决底部白色部分
+            .ignoresSafeArea(.all,edges: .bottom)
         }
     }
 }
--- IdleFish/IdleFish/CustomTabbar/CustomTabbarView.swift
+++ IdleFish/IdleFish/CustomTabbar/CustomTabbarView.swift
@@ -40,6 +40,9 @@ enum CustomTabbar: String, CaseIterable {
 }
 
 struct CustomTabbarView: View {
+    // 传入安全距离
+    var safeEdgeInsets: EdgeInsets
+
     // @Binding 表示当该变量改变时,会通知外部属性,也就是说这个值由外部传递进来的
     @Binding var currTab: CustomTabbar
 
@@ -66,10 +69,55 @@ struct CustomTabbarView: View {
 
                 // 让tabbarItem宽度等分
                 .frame(maxWidth: .infinity)
-                
-                //接下来插入发布闲置的按钮
+
+                // 接下来插入发布闲置的按钮
+                if item == .niceplay {
+                    Button {
+                    } label: {
+                        Image("tabbar_post_idle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 75, height: 75)
+                        // 重新制定按钮图片的大小
+                    }
+                    // 让发布按钮向上10
+                    .offset(y: -10)
+                }
             }
         }
+        .padding(.top, 10)
+        // 添加边距,.bottom表示添加底部边距,考虑到有些机型不需要这个间距,添加一个默认间距
+        .padding(.bottom, safeEdgeInsets.bottom == 0 ? 10 : safeEdgeInsets.bottom)
+        // 接下来修改背景色,实现圆弧效果
+//        .background(Color.orange)
+        .background(
+            // Canvas是SwiftUI引入的新特性,仅支持iOS15以上版本
+            Canvas { context, size in
+
+                let width = size.width
+                let height = size.height
+                let midWidth = width * 0.5
+
+                context.fill(Path { path in
+
+                    //  先绘制一个矩形
+                    //  接下来利用贝塞尔曲线实现,实现发布闲置按钮的凸起
+                    path.move(to: CGPoint(x: 0, y: 20))
+
+                    path.addLine(to: CGPoint(x: midWidth - 55, y: 20))
+
+                    path.addCurve(to: CGPoint(x: midWidth, y: 0), control1: CGPoint(x: midWidth - 35, y: 20), control2: CGPoint(x: midWidth - 15, y: 0))
+
+                    path.addCurve(to: CGPoint(x: midWidth + 55, y: 20), control1: CGPoint(x: midWidth + 15, y: 0), control2: CGPoint(x: midWidth + 35, y: 20))
+
+                    path.addLine(to: CGPoint(x: size.width, y: 20))
+
+                    path.addLine(to: CGPoint(x: size.width, y: size.height))
+                    path.addLine(to: CGPoint(x: 0, y: size.height))
+
+                }, with: .color(.white))
+            }
+        )
     }
 }
 
