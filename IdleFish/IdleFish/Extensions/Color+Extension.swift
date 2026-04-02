a94eaa8b0e7d9a66c00d2aa93455220041fb22a5
卤蛋君 <76891972@qq.com>
2026-04-02 10:51:01 +0800

    add:添加定位

---
+++ IdleFish/IdleFish/Extensions/Color+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extension.swift
+//  IdleFish
+//
+//  Created by Mac Mini on 2026/4/1.
+//
+
+import Foundation
---
+++ IdleFish/IdleFish/Library/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  IdleFish
+//
+//  Created by Mac Mini on 2026/4/1.
+//
+
+import Foundation
